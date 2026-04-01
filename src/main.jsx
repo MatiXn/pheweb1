3a4d2d347c5ce0b70dce5e2aee2da9164c6bd934
Matien <matin.askaryar@phe-perm.de>
2026-04-01 21:56:09 +0200

    fix main routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import VacancyFunnelPage from "./pages/VacancyFunnelPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -10,6 +13,9 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/funnel/:slug" element={<VacancyFunnelPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
